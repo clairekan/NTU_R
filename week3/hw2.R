1ca43932c51b18ce4f50622056afabe25fbee5b1
clairekan <40970135+clairekan@users.noreply.github.com>
2018-07-21 17:32:48 +0800

    hw2

---
+++ week3/hw2.R
@@ -0,0 +1,45 @@
+library(tm)
+library(tmcn)
+library(factoextra)
+library(Matrix)
+library(NLP)
+
+music.corpus <- Corpus( DirSource("./musical") )
+#DirSource是Directory Source顧名思義就是指定一個資料夾作為資料來源所以參數是要放一個資料夾的路徑
+#開始清理資料
+music.corpus <- tm_map(music.corpus, removePunctuation)
+music.corpus <- Corpus(VectorSource(music.corpus))
+music.corpus<- tm_map(music.corpus, content_transformer(tolower))
+music.tdm <- TermDocumentMatrix(music.corpus, control = list())
+inspect(music.tdm)
+
+docs.tf <- apply(as.matrix(music.tdm), 2, function(word) { word/sum(word) })
+idf <- function(doc) {
+  return ( log2( length(doc)+1 / nnzero(doc)) )
+}
+docs.idf <- apply(as.matrix(music.tdm), 1, idf)
+docs.tfidf <- docs.tf * docs.idf
+tail(docs.tfidf)
+
+docs.pca <- prcomp(docs.tfidf, scale = T)
+
+fviz_eig(docs.pca)
+
+fviz_pca_ind(docs.pca, geom.ind = c("point"), col.ind = "cos2")
+
+fviz_pca_var(docs.pca, col.var = "contrib")
+
+fviz_pca_biplot(docs.pca, geom.ind = "point")
+
+docs.eig <- get_eig(docs.pca)
+docs.var <- get_pca_var(docs.pca)
+docs.ind <- get_pca_ind(docs.pca)
+
+ind.coord2 <- docs.ind$coord[, 1:2]
+wss <- c()
+for (i in 1:10) { wss[i] <- kmeans(ind.coord2, i)$tot.withinss }
+plot(wss, type = "b")
+
+km <- kmeans(ind.coord2, 3)
+plot(ind.coord2, col = km$cluster)
+points(km$centers, col = 1:3, pch = 8, cex = 2)
